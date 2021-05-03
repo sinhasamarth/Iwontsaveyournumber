1054b8ed613420b3948fdecba5387c5b4e9207ee
Saksham Singh <sakshamsingh01@outlook.com>
2021-05-03 13:38:30 +0530

    Initial

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect, url_for
+import phonenumbers
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('index.html', valid = request.args.get('valid'))
+
+@app.route('/validate', methods=['GET', 'POST'])
+def validate():
+    entry = request.args.get('phone')
+    try:
+        number = phonenumbers.is_valid_number(phonenumbers.parse(entry, None))
+        return redirect (f'http://api.whatsapp.com/send?phone={entry}')
+    except:
+        return redirect(url_for('home', valid = 0))
+
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)    
